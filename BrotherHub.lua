cbd888a73dab092ea89e920c13a265d2a33a8560
prawiraxliv <prawirabayu2026@gmail.com>
2026-09-16 04:16:07 +0800

    feat(drill): update Drill to Earth build v2.0 - manual tool fix, stable combat, config saving, vector mincircle

--- BrotherHub.lua
+++ BrotherHub.lua
@@ -256,7 +256,7 @@ local HUB_ROUTER = {
             if rs:FindFirstChild("DrillService") or (rs:FindFirstChild("Packages") and rs.Packages:FindFirstChild("Knit") and rs:FindFirstChild("ToolService")) then return true end
             return false
         end,
-        CdnUrl = "https://files.catbox.moe/1y0c0x.lua",
+        CdnUrl = "https://files.catbox.moe/py1736.lua",
         GitHubUrl = "https://raw.githubusercontent.com/brotherhub-official/BROTHERHUB/main/DrillToEarth_BROTHERHUB.lua",
     },
     {
